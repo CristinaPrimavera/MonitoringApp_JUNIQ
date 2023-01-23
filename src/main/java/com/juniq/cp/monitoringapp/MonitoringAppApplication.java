ea36a2edbd39ce027c9e712bb7032f18e5e0e8e4
CristinaPrimavera <primaveracristina@gmail.com>
2023-01-23 18:27:08 +0100

    Enable push for async tasks

--- src/main/java/com/juniq/cp/monitoringapp/MonitoringAppApplication.java
+++ src/main/java/com/juniq/cp/monitoringapp/MonitoringAppApplication.java
@@ -1,10 +1,20 @@
 package com.juniq.cp.monitoringapp;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.shared.communication.PushMode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class MonitoringAppApplication {
+@SpringBootApplication()
+@Push(PushMode.AUTOMATIC)
+public class MonitoringAppApplication extends SpringBootServletInitializer implements AppShellConfigurator {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MonitoringAppApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonitoringAppApplication.class, args);
